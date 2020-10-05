f85567b3c1cc635fd0a2be05253e665d9cce5e2e
Steve McDougall <juststevemcd@gmail.com>
2020-10-05 10:49:33 +0100

    Load Menu information from a Menu service instead of directly from config

--- app/Services/Menu/MenuLoader.php
+++ app/Services/Menu/MenuLoader.php
@@ -1,10 +1,21 @@
-<?php
-
+<?php declare(strict_types=1);
 
 namespace App\Services\Menu;
 
-
 class MenuLoader
 {
+    public static function main(): array
+    {
+        return config('phponline.nav.main');
+    }
 
+    public static function user(): array
+    {
+        return config('phponline.nav.user');
+    }
+
+    public static function footer(): array
+    {
+        return config('phponline.nav.footer');
+    }
 }
