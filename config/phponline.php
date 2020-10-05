c5d6b02bed6a858c67d6bafaf59b6313ce35e2b0
Steve McDougall <juststevemcd@gmail.com>
2020-10-05 10:48:51 +0100

    Adding nav to config

--- config/phponline.php
+++ config/phponline.php
@@ -12,4 +12,40 @@ return [
 
         'admin' => 'phponlinedev@gmail.com',
     ],
+
+    'nav' => [
+
+        'main' => [
+
+            [
+                'name' => 'Blog',
+                'link' => '/blog',
+                'title' => 'View our Blog',
+                'pattern' => 'articles:index'
+            ],
+
+            [
+                'name' => 'Tutorials',
+                'link' => '/category/tutorials',
+                'title' => 'View our Tutorials',
+                'pattern' => 'categories:show'
+            ],
+
+            [
+                'name' => 'Packages',
+                'link' => '/packages',
+                'title' => 'View our Packages',
+                'pattern' => 'packages:index'
+            ],
+
+            [
+                'name' => 'User Groups',
+                'link' => '/user-groups',
+                'title' => 'View our User Groups',
+                'pattern' => 'user-groups:index'
+            ]
+        ],
+
+        'footer' => []
+    ]
 ];
